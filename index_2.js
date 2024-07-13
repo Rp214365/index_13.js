be1c2e42440f767621d3268b5c759e7ea3a940db
Rp214365 <patelricky7801@gmail.com>
2024-07-13 20:35:14 +0530

    Add files via upload

---
+++ index_2.js
@@ -0,0 +1,21 @@
+// let a = 0
+// while (a <= 10) {
+//     console.log(`Value of a is ${a}`);
+//     a = a+2;
+// }
+
+// let MyArray = ["flasu", "batman" , "Memsn"];
+
+// let arr = 0;
+
+// while (arr < MyArray.length) {
+//     console.log(`Vale is ${MyArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+let score = 1;
+
+do {
+    console.log(`scroe  is${score}`);
+    score++
+} while (score<=10);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let MyArray = ["flasu", "batman" , "Memsn"];
+
+let arr = 0;
+
+while (arr < MyArray.length) {
+    console.log(`Vale is ${MyArray[arr]}`);
+    arr = arr + 1;
+}
